8c8f2ba4489cbd3dbfcf72ad623ef8fa4139aee1
Sergio <oliv.serg@gmail.com>
2025-06-27 22:44:47 +0200

    Update ean-mapping.js

--- ean-mapping.js
+++ ean-mapping.js
@@ -1,3 +1,67 @@
-tu peux les intégrer au ean-mapping ?
-
-
+const eanMapping = {
+  "7640428083966": {
+    reference: "10255 357RM BUIDR",
+    collection: "CO-1",
+    description: "CO-1 Chronorally Chronograph"
+  },
+  "7640428083826": {
+    reference: "10255 3BCAB BUCIDN",
+    collection: "CO-1",
+    description: "CO-1 Chronorally Chronograph"
+  },
+  "7640428084333": {
+    reference: "10255 3BM NADN",
+    collection: "CO-1",
+    description: "CO-1 Chronorally Chronograph"
+  },
+  "7640428083410": {
+    reference: "10255 37RCA BRDR",
+    collection: "CO-1",
+    description: "CO-1 Chronorally Chronograph"
+  },
+  "7640428083403": {
+    reference: "10255 37RBCAB BEIDR",
+    collection: "CO-1",
+    description: "CO-1 Chronorally Chronograph"
+  },
+  "7640428080418": {
+    reference: "38003 TINGNOCAO GNO",
+    collection: "Chronorally",
+    description: "Chronorally Chronograph"
+  },
+  "7640428080413": {
+    reference: "38003 TINR CANBR",
+    collection: "Chronorally",
+    description: "Chronorally Chronograph"
+  },
+  "7640428084326": {
+    reference: "80131 357NRNM VIO",
+    collection: "SkyDiver",
+    description: "SkyDiver 38 Date Automatic"
+  },
+  "7640428084395": {
+    reference: "80131 37RNM NI",
+    collection: "SkyDiver",
+    description: "SkyDiver 38 Date Automatic"
+  },
+  "7640428084331": {
+    reference: "80131 37RNM VDBEI",
+    collection: "SkyDiver",
+    description: "SkyDiver 38 Date Automatic"
+  },
+  "7640428084320": {
+    reference: "80131 3NM VI",
+    collection: "SkyDiver",
+    description: "SkyDiver 38 Date Automatic"
+  },
+  "7640428084322": {
+    reference: "80131 3NM NDRO",
+    collection: "SkyDiver",
+    description: "SkyDiver 38 Date Automatic"
+  },
+  "7640428084342": {
+    reference: "80128 37NLNM NLN",
+    collection: "Hydro-sub",
+    description: "Hydro-Sub Date Chronometer Automatic – Label Noir Limited Edition"
+  }
+};
